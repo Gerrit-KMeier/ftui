a6fb81f45022ae746035bc086653e10504d80a40
Thyraz <tobias.wiedenmann@me.com>
2020-11-24 16:10:50 +0100

    Fix for Calendar refreshing before sub-elements are loaded

--- components/calendar/calendar.component.js
+++ components/calendar/calendar.component.js
@@ -14,6 +14,7 @@
 */
 
 import { FtuiElement } from '../element.component.js';
+import { FtuiCalendarData } from './calendar-data.component.js';
 import  { FullCalendar }  from '../../modules/fullcalendar/main.js';
 
 export class FtuiCalendar extends FtuiElement {
